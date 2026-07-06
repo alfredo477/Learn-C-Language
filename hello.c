16769c4a9087ac6d35b90b969dbf7a461e4d7978
wonder-jpg <1272934089@qq.com>
2026-07-06 17:50:39 +0800

    Update hello message

--- hello.c
+++ hello.c
@@ -1,5 +1,9 @@
+deb http://mirrors.aliyun.com/ubuntu/ noble main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ noble-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ noble-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ noble-backports main restricted universe multiverse
 #include<stdio.h>
 int main(){
-	printf("Hello,C Language!I am ready.\n");
+	printf("Hello,Git is amazing.\n");
 	return 0;
 	}
