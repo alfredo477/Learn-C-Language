53f5a27567526717a6e60b808671ca716d09c064
alfredo447 <1272934089@qq.com>
2026-07-19 21:02:41 +0800

    更新

--- 06.break-and-continue/03.continue.c
+++ 06.break-and-continue/03.continue.c
@@ -1,12 +1,12 @@
-//comtinue代表继续的意思 如果在循环中代表循环继续
+﻿//continue代表继续的意思 如果在循环中代表循环继续
 #include<stdio.h>
 int main(){
-    int i=1;
+    int i=0;
     while(i<=10){
         if(i==5)
             continue;
+        i++;
         printf("%d",i);
-        i=i+1;
     }
     return 0;
 }
